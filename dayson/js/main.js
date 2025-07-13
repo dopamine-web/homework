9f71e3323dfdb9d89f86a3006f4317c76d67f457
dopamine-web <urgand.anna@mail.ru>
2025-07-13 21:33:46 +0300

    Add files via upload

---
+++ dayson/js/main.js
@@ -0,0 +1,11 @@
+document.querySelector('.header__burger').addEventListener('click', function () {
+        document.body.classList.toggle('body--opened-menu');
+    });
+
+
+
+
+
+
+    var mixer = mixitup('.products-grid')
+
